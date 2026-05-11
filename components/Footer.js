a9ca0a9ce5e99a9debfdab6b7cc4241626df1eca
ziggs <cayliseseveneleven@gmail.com>
2026-05-11 19:10:28 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -8,8 +8,8 @@ export default function Footer() {
         <div className={styles.footerBrand}>
           DC Invs
           <br />
-          <span>({" "}
-          <Link href="/zigydbot" className="underline decoration-white/30 underline-offset-4 transition-colors duration-300 hover:text-white/85">
+          <span>(
+          <Link href="https://www.ziggymc.me/zigydbot" className="underline decoration-white/30 underline-offset-4 transition-colors duration-300 hover:text-white/85">
             zigydbot
           </Link>)</span>
         </div>
